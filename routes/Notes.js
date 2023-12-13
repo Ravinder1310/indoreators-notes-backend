a3f6232d1f3d41e0b9c63cfd90fc693d1334abb3
Ravinder1310 <105969256+Ravinder1310@users.noreply.github.com>
2023-12-13 18:18:07 +0530

    backend created successfully for managing notes

---
+++ routes/Notes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { createNotesController, deleteNotesController, getNotesController } from "../controllers/ArticlesController.js";
+
+
+const router = express.Router();
+
+// get notes
+router.get("/get-notes", getNotesController);
+
+// create notes
+router.post("/create-notes", createNotesController);
+
+// delete notes
+router.delete("/delete-notes/:nid", deleteNotesController);
+
+export default router;
